dd8955324f1aae104b10d4336cec42fa64a01d76
Joao Victor Costa <victor_ngl2@hotmail.com>
2023-01-09 11:09:07 -0300

    Criacao dos json e permissoes para cada modalidade

--- pages/RematriculaForm.js
+++ pages/RematriculaForm.js
@@ -1,6 +1,46 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router'
 
+const OficinasEsportivas = [
+  {id: 1, name: 'BASQUETE - 3º ao 5º /EF', permission: [{serie: '3º Ano'}, {serie: '5º Ano'}, {serie: '5º Ano'} ]},
+  {id: 2, name: 'BASQUETE - 6º ao 8º /EF', permission: [{serie: '6º Ano'}, {serie: '7º Ano'}, {serie: '8º Ano'} ]},
+  {id: 3, name: 'BASQUETE - 9º/EF ao 3º E. Médio', permission: [{serie: '9º Ano'}, {serie: '1ª Série'}, {serie: '2ª Série'}, {serie: '3ª Série'} ]},
+  {id: 4, name: 'CAPOEIRA - 1º e 2º /EF', permission: [{serie: '1º Ano'}, {serie: '2º Ano'} ]},
+  {id: 5, name: 'CAPOEIRA - 3º /EF', permission: [{serie: '3º Ano'}]},
+  {id: 6, name: 'CAPOEIRA - 4º e 5º /EF', permission: [{serie: '4º Ano'}, {serie: '5º Ano'} ]},
+  {id: 7, name: 'CAPOEIRA - 6º ao 9º /EF', permission: [{serie: '6º Ano'}, {serie: '7º Ano'}, {serie: '8º Ano'} ]},
+  {id: 8, name: 'ESCOLA DA BOLA - 1º /EF', permission: [{serie: '1º Ano'}]},
+  {id: 9, name: 'ESCOLA DA BOLA - 2º/EF', permission: [{serie: '2º Ano'}]},
+  {id: 10, name: 'FUTSAL (masc) - 5º /EF', permission: [{serie: '5º Ano'}]},
+  {id: 11, name: 'FUTSAL (masc) - 6º e 7º /EF', permission: [{serie: '6º Ano'}, {serie: '7º Ano'} ]},
+  {id: 12, name: 'FUTSAL (masc) - 8º e 9º /EF', permission: [{serie: '8º Ano'}, {serie: '9º Ano'} ]},
+  {id: 13, name: 'FUTSAL (masc) - E. Médio', permission: [{serie: '1ª Série'}, {serie: '2ª Série'}, {serie: '3ª Série'} ]},
+  {id: 14, name: 'FUTSAL (misto) - 1º e 2º / EF', permission: [{serie: '1º Ano'}, {serie: '2º Ano'} ]},
+  {id: 15, name: 'FUTSAL (misto)- 3º e 4º EF', permission: [{serie: '3º Ano'}, {serie: '4º Ano'} ]},
+  {id: 16, name: 'HANDEBOL - 3º ao 5º /EF', permission: [{serie: '3º Ano'}, {serie: '4º Ano'}, {serie: '5º Ano'} ]},
+  {id: 17, name: 'HANDEBOL - 6º ao 8º /EF', permission: [{serie: '6º Ano'}, {serie: '7º Ano'}, {serie: '8º Ano'} ]},
+  {id: 18, name: 'HANDEBOL - 9º /EF ao 3º/E. Médio', permission: [{serie: '9º Ano'}, {serie: '1ª Série'}, {serie: '2ª Série'}, {serie: '3ª Série'} ]},
+  {id: 19, name: 'JUDÔ - 3º e 4º / EF', permission: [{serie: '3º Ano'}, {serie: '4º Ano'} ]},
+  {id: 20, name: 'JUDÔ - 5º e 6 º /EF', permission: [{serie: '5º Ano'}, {serie: '6º Ano'} ]},
+  {id: 21, name: 'JUDÔ - 7º ao 9º /EF', permission: [{serie: '7º Ano'}, {serie: '8º Ano'}, {serie: '9º Ano'} ]},
+  {id: 22, name: 'JUDÔ -1° e 2º /EF', permission: [{serie: '1º Ano'}, {serie: '2º Ano'} ]},
+  {id: 23, name: 'VOLEIBOL - 3º ao 5ª /EF', permission: [{serie: '3º Ano'}, {serie: '4º Ano'}, {serie: '5º Ano'} ]},
+  {id: 24, name: 'VOLEIBOL - 6º ao 9º /EF', permission: [{serie: '6º Ano'}, {serie: '7º Ano'}, {serie: '8º Ano'}, {serie: '9º Ano'} ]},
+  {id: 25, name: 'XADREZ - 2º ao 4º /EF', permission: [{serie: '2º Ano'}, {serie: '3º Ano'}, {serie: '4º Ano'} ]},
+  {id: 26, name: 'XADREZ - 5º ao 8º /EF', permission: [{serie: '5º Ano'}, {serie: '6º Ano'}, {serie: '7º Ano'}, {serie: '8º Ano'} ]},
+  
+];
+
+
+const OficinasCulturais = [
+  {id: 1, name: 'Coral - 1º e 2º /EF', permission: [{serie: '1º Ano'}, {serie: '2º Ano'}]},
+  {id: 2, name: 'Coral -  3º /EF ao E. Médio', permission: [{serie: '3º Ano'}, {serie: '4º Ano'}, {serie: '5º Ano'}, {serie: '6º Ano'}, {serie: '7º Ano'}, {serie: '8º Ano'}, {serie: '9º Ano'}, {serie: '1º Série'}, {serie: '2ª Série'}, {serie: '3ª Série'} ]},
+  {id: 3, name: 'Flauta - 2º ao 5º /EF', permission: [{serie: '2º Ano'}, {serie: '3º Ano'}, {serie: '4º Ano'}, {serie: '5º Ano'} ]},
+  {id: 4, name: 'Ukulele - 5º /EF ao E. MédioF', permission: [{serie: '5º Ano'}, {serie: '6º Ano'}, {serie: '7º Ano'}, {serie: '8º Ano'}, {serie: '9º Ano'}, {serie: '1º Série'}, {serie: '2ª Série'}, {serie: '3ª Série'} ]},
+  {id: 5, name: 'Desenho Artístico - 4º e 5º /EF', permission: [{serie: '4º Ano'}, {serie: '5º Ano'}]},
+  {id: 6, name: 'Desenho Artístico - 6º /EF ao E. Médio', permission: [ {serie: '6º Ano'}, {serie: '7º Ano'}, {serie: '8º Ano'}, {serie: '9º Ano'}, {serie: '1º Série'}, {serie: '2ª Série'}, {serie: '3ª Série'} ]},
+];
+
 export default function RematriculaForm({ responsavel, setResponsavel }) {
   const [AtividadeCultural, setAtividadeCultural] = useState('0');
   const [AtividadeEsportiva, setAtividadeEsportiva] = useState('0');
@@ -8,8 +48,6 @@ export default function RematriculaForm({ responsavel, setResponsavel }) {
   const [AtividadePrioridade, setAtividadePrioridade] = useState('0');
   const router = useRouter()
 
-  const link = 'https://cambauba.org.br/matricula_edital_interno/contratos/Contrato%20Renova%C3%A7%C3%A3o%202023%20-%20Wpensar.pdf'
- 
   function isEmptyObject(obj) {
     for (var key in obj) {
       if (Object.prototype.hasOwnProperty.call(obj, key)) {
@@ -69,11 +107,13 @@ export default function RematriculaForm({ responsavel, setResponsavel }) {
   if (isEmptyObject(responsavel))
     return (<h1>Erro desconhecido</h1>)
 
+    console.log(OficinasEsportivas)
+
   return (
     <div className='p-5'>
       <form onSubmit={handleSubmit}>
         <div className='text-sm text-justify mb-6'>Prezado (a) Associado(a), o presente questionário tem como objetivo agilizar o processo
-          de renovação de matrícula do aluno para o ano letivo de 2023. Inicialmente, cumpre informar que a renovação da matrícula será realizada totalmente
+          de renovação de matrícula do aluno para o Ano letivo de 2023. Inicialmente, cumpre informar que a renovação da matrícula será realizada totalmente
           de forma online.</div>
         <div className="relative z-0 mb-6 w-full group">
           <input value={responsavel.aluno.nome} type="name" name="nome" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " disabled />
@@ -91,16 +131,16 @@ export default function RematriculaForm({ responsavel, setResponsavel }) {
           <div onChange={onPrioridadeValueChange}>
             <div className='mb-2'>Deseja que a primeira opção seja?</div>
             <fieldset className='flex gap-10 mb-5' >
-              <div className="flex items-center mb-4" >
-                <input required id="country-option-1" type="radio" name="countries" defaultValue="Cultural" className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" />
-                <label htmlFor="country-option-1" className="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                  Cultural
-                </label>
-              </div>
               <div className="flex items-center mb-4">
                 <input id="country-option-2" type="radio" name="countries" defaultValue="Esportiva" className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" />
                 <label htmlFor="country-option-2" className="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                   Esportiva
+                </label>
+              </div>
+              <div className="flex items-center mb-4" >
+                <input required id="country-option-1" type="radio" name="countries" defaultValue="Cultural" className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" />
+                <label htmlFor="country-option-1" className="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Cultural
                 </label>
               </div>
             </fieldset>
@@ -108,51 +148,38 @@ export default function RematriculaForm({ responsavel, setResponsavel }) {
 
       
           <div className='mb-5' onChange={onEsportivaValueChange}>
-            <label required htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Escolha uma atividade esportiva: </label>
-            <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <label required htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Escolha uma atividade esportiva: {AtividadePrioridade == 'Esportiva' && <h3 className='font-bold'> Atividade Prioritária </h3>}</label>
+            
+            
+           <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+           <option value="Não se aplica">Não se aplica</option> 
 
-              <option value="Não se aplica">Não se aplica</option> 
-              <option value="BASQUETE - 3º ao 5º /EF">BASQUETE - 3º ao 5º /EF</option> 
-              <option value="BASQUETE - 6º ao 8º /EF">BASQUETE - 6º ao 8º /EF</option>
-              <option value="BASQUETE - 9º/EF ao 3º E. Médio">BASQUETE - 9º/EF ao 3º E. Médio</option>
-              <option value="CAPOEIRA - 1º e 2º /EF">CAPOEIRA - 1º e 2º /EF</option>
-              <option value="CAPOEIRA - 3º /EF">CAPOEIRA - 3º /EF</option>
-              <option value="CAPOEIRA - 4º e 5º /EF">CAPOEIRA - 4º e 5º /EF</option>
-              <option value="CAPOEIRA - 6º ao 9º /EF">CAPOEIRA - 6º ao 9º /EF</option>
-              <option value="ESCOLA DA BOLA - 1º /EF">ESCOLA DA BOLA - 1º /EF</option>
-              <option value="ESCOLA DA BOLA - 2º/EF">ESCOLA DA BOLA - 2º/EF</option>
-              <option value="FUTSAL (masc) - 5º /EF">FUTSAL (masc) - 5º /EF</option>
-              <option value="FUTSAL (masc) - 6º e 7º /EF">FUTSAL (masc) - 6º e 7º /EF</option>
-              <option value="FUTSAL (masc) - 8º e 9º /EF">FUTSAL (masc) - 8º e 9º /EF</option>
-              <option value="FUTSAL (masc) - E. Médio">FUTSAL (masc) - E. Médio</option>
-              <option value="FUTSAL (misto) - 1º e 2º / EF">FUTSAL (misto) - 1º e 2º / EF</option>
-              <option value="FUTSAL (misto)- 3º e 4º EF">FUTSAL (misto)- 3º e 4º EF</option>
-              <option value="HANDEBOL - 3º ao 5º /EF">HANDEBOL - 3º ao 5º /EF</option>
-              <option value="HANDEBOL - 6º ao 8º /EF">HANDEBOL - 6º ao 8º /EF</option>
-              <option value="HANDEBOL - 9º /EF ao 3º/E. Médio">HANDEBOL - 9º /EF ao 3º/E. Médio</option>
-              <option value="JUDÔ - 3º e 4º / EF">JUDÔ - 3º e 4º / EF</option>
-              <option value="JUDÔ - 5º e 6 º /EF">JUDÔ - 5º e 6 º /EF</option>
-              <option value="JUDÔ - 7º ao 9º /EF">JUDÔ - 7º ao 9º /EF</option>
-              <option value="JUDÔ -1° e 2º /EF">JUDÔ -1° e 2º /EF</option>
-              <option value="VOLEIBOL - 3º ao 5ª /EF">VOLEIBOL - 3º ao 5ª /EF</option>
-              <option value="VOLEIBOL - 6º ao 9º /EF">VOLEIBOL - 6º ao 9º /EF</option>
-              <option value="XADREZ - 2º ao 4º /EF">XADREZ - 2º ao 4º /EF</option>
-              <option value="XADREZ - 5º ao 8º /EF">XADREZ - 5º ao 8º /EF</option>
+            {OficinasEsportivas.map((modalidade, index) => 
+
+              modalidade.permission.map((permissao, index) => (
               
-
+              permissao.serie == responsavel.aluno.s_rie && <option key={index} value={modalidade.name}>{modalidade.name}</option>)) 
+             
+             )}
+             
             </select>
           </div>
 
           <div className='mb-5' onChange={onCulturalValueChange}>
-            <label required htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Escolha uma atividade cultural: </label>
+            <label required htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Escolha uma atividade cultural: {AtividadePrioridade == 'Cultural' && <h3 className='font-bold'> Atividade Prioritária </h3>}</label>
             <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-              <option value="Não se aplica">Não se aplica</option> 
-              <option value="Coral - 1º e 2º /EF">Coral - 1º e 2º /EF</option> 
-              <option value="Coral -  3º /EF ao E. Médio">Coral -  3º /EF ao E. Médio</option>
-              <option value="Flauta - 2º ao 5º /EF">Flauta - 2º ao 5º /EF</option>
-              <option value="Ukulele - 5º /EF ao E. Médio">Ukulele - 5º /EF ao E. Médio</option>
-              <option value="Desenho Artístico - 4º e 5º /EF">Desenho Artístico - 4º e 5º /EFF</option>
-              <option value="Desenho Artístico - 6º /EF ao E. Médio">Desenho Artístico - 6º /EF ao E. Médio</option>
+            
+            <option value="Não se aplica">Não se aplica</option> 
+
+            {OficinasCulturais.map((modalidade, index) => 
+
+                  modalidade.permission.map((permissao, index) => (
+
+                  permissao.serie == responsavel.aluno.s_rie && <option key={index} value={modalidade.name}>{modalidade.name}</option>)) 
+
+              )}
+
+              
               
             
             </select>
@@ -162,45 +189,29 @@ export default function RematriculaForm({ responsavel, setResponsavel }) {
             <label required htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Escolha uma atividade optativa: </label>
             <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <option value="Não se aplica">Não se aplica</option> 
-              <option value="BASQUETE - 3º ao 5º /EF">BASQUETE - 3º ao 5º /EF</option> 
-              <option value="BASQUETE - 6º ao 8º /EF">BASQUETE - 6º ao 8º /EF</option>
-              <option value="BASQUETE - 9º/EF ao 3º E. Médio">BASQUETE - 9º/EF ao 3º E. Médio</option>
-              <option value="CAPOEIRA - 1º e 2º /EF">CAPOEIRA - 1º e 2º /EF</option>
-              <option value="CAPOEIRA - 3º /EF">CAPOEIRA - 3º /EF</option>
-              <option value="CAPOEIRA - 4º e 5º /EF">CAPOEIRA - 4º e 5º /EF</option>
-              <option value="CAPOEIRA - 6º ao 9º /EF">CAPOEIRA - 6º ao 9º /EF</option>
-              <option value="ESCOLA DA BOLA - 1º /EF">ESCOLA DA BOLA - 1º /EF</option>
-              <option value="ESCOLA DA BOLA - 2º/EF">ESCOLA DA BOLA - 2º/EF</option>
-              <option value="FUTSAL (masc) - 5º /EF">FUTSAL (masc) - 5º /EF</option>
-              <option value="FUTSAL (masc) - 6º e 7º /EF">FUTSAL (masc) - 6º e 7º /EF</option>
-              <option value="FUTSAL (masc) - 8º e 9º /EF">FUTSAL (masc) - 8º e 9º /EF</option>
-              <option value="FUTSAL (masc) - E. Médio">FUTSAL (masc) - E. Médio</option>
-              <option value="FUTSAL (misto) - 1º e 2º / EF">FUTSAL (misto) - 1º e 2º / EF</option>
-              <option value="FUTSAL (misto)- 3º e 4º EF">FUTSAL (misto)- 3º e 4º EF</option>
-              <option value="HANDEBOL - 3º ao 5º /EF">HANDEBOL - 3º ao 5º /EF</option>
-              <option value="HANDEBOL - 6º ao 8º /EF">HANDEBOL - 6º ao 8º /EF</option>
-              <option value="HANDEBOL - 9º /EF ao 3º/E. Médio">HANDEBOL - 9º /EF ao 3º/E. Médio</option>
-              <option value="JUDÔ - 3º e 4º / EF">JUDÔ - 3º e 4º / EF</option>
-              <option value="JUDÔ - 5º e 6 º /EF">JUDÔ - 5º e 6 º /EF</option>
-              <option value="JUDÔ - 7º ao 9º /EF">JUDÔ - 7º ao 9º /EF</option>
-              <option value="JUDÔ -1° e 2º /EF">JUDÔ -1° e 2º /EF</option>
-              <option value="VOLEIBOL - 3º ao 5ª /EF">VOLEIBOL - 3º ao 5ª /EF</option>
-              <option value="VOLEIBOL - 6º ao 9º /EF">VOLEIBOL - 6º ao 9º /EF</option>
-              <option value="XADREZ - 2º ao 4º /EF">XADREZ - 2º ao 4º /EF</option>
-              <option value="XADREZ - 5º ao 8º /EF">XADREZ - 5º ao 8º /EF</option>
-              <option value="Coral - 1º e 2º /EF">Coral - 1º e 2º /EF</option> 
-              <option value="Coral -  3º /EF ao E. Médio">Coral -  3º /EF ao E. Médio</option>
-              <option value="Flauta - 2º ao 5º /EF">Flauta - 2º ao 5º /EF</option>
-              <option value="Ukulele - 5º /EF ao E. Médio">Ukulele - 5º /EF ao E. Médio</option>
-              <option value="Desenho Artístico - 4º e 5º /EF">Desenho Artístico - 4º e 5º /EFF</option>
-              <option value="Desenho Artístico - 6º /EF ao E. Médio">Desenho Artístico - 6º /EF ao E. Médio</option>
+
+              {OficinasEsportivas.map((modalidade, index) => 
+
+              modalidade.permission.map((permissao, index) => (
+              
+              permissao.serie == responsavel.aluno.s_rie && <option key={index} value={modalidade.name}>{modalidade.name}</option>)) 
+             
+             )}
+
+              {OficinasCulturais.map((modalidade, index) => 
+
+              modalidade.permission.map((permissao, index) => (
+
+              permissao.serie == responsavel.aluno.s_rie && <option key={index} value={modalidade.name}>{modalidade.name}</option>)) 
+
+              )}
               
             
             </select>
           </div>
           
         <div className='text-sm text-justify mb-6'>Em caso de modificação de algum dado cadastral dos responsáveis que
-          assinaram o contrato para o ano de 2022, solicitamos, por gentileza, a sinalização com o
+          assinaram o contrato para o Ano de 2022, solicitamos, por gentileza, a sinalização com o
           encaminhamento dos novos dados para o e-mail: secretaria@cambauba.org.br. <div className='mt-2'>Na hipótese de dúvidas, estaremos à disposição para esclarecimentos através do e-mail: matriculas2023@cambauba.org.br</div></div>
         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Enviar</button>
       </form>
